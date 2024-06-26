a544c7764f6456455822eb6f948a4fdfd1093b3a
Anvar Adizov <anvaradizov100@gmail.com>
2024-06-26 16:57:38 +0500

    before header animation

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sun = document.querySelector('.sun');
+    const king = document.querySelector('.king');
+    const queen = document.querySelector('.queen');
+    const knight = document.querySelector('.knight');
+    const pawn = document.querySelector('.pawn');
+
+    sun.style.animation = 'riseSun 2s ease-out';
+    king.style.animation = 'riseChessPieces 1.5s ease-out 0.5s forwards';
+    queen.style.animation = 'riseChessPieces 1.5s ease-out 0.5s forwards';
+
+    setTimeout(() => {
+        knight.style.animation = 'tiltPieces 1s ease-in-out forwards';
+        pawn.style.animation = 'tiltPieces 1s ease-in-out forwards';
+    }, 1000);
+});
